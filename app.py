c24a6c5041c2c5c11c9b44ddfb51f1e40f14c439
Sidhowes <sidhowes@mail.com>
2020-10-28 13:49:52 +0000

    add_recipe template added and materialize form structure on place.

--- app.py
+++ app.py
@@ -27,6 +27,11 @@ def get_recipes():
     return render_template("recipes.html", recipes=recipes)
 
 
+@app.route("/add_recipe")
+def add_recipe():
+    return render_template("add_recipe.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), 
             port=int(os.environ.get("PORT")),

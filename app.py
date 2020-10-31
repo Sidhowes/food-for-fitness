fb39642a1ad42ce01ea1d73b3dfbc25abdefff64
Sidhowes <sidhowes@mail.com>
2020-10-31 10:48:39 +0000

    User can add recipes to db and home page

--- app.py
+++ app.py
@@ -16,19 +16,30 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+
 @app.route("/")
-def index():
-    return render_template("index.html")
-
-
 @app.route("/get_recipes")
 def get_recipes():
     recipes = list(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=recipes)
 
 
-@app.route("/add_recipe")
+@app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
+    if request.method == "POST":
+        recipe = {
+            "recipe_name": request.form.get("recipe_name"),
+            "category_name": request.form.get("category_name"),
+            "recipe_description": request.form.get("recipe_description"),
+            "img_url": request.form.get("img_url"),
+            "cook_time": request.form.get("cook_time"),
+            "ingredients": request.form.get("ingredients"),
+            "instructions": request.form.get("instructions"),
+        }
+        mongo.db.recipes.insert_one(recipe)
+        flash("Recipe Added Successfully")
+        return redirect(url_for("get_recipes"))
+
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_recipe.html", categories=categories)
 

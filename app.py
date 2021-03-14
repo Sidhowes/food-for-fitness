d064501d0fa0a13db940675293a15dfa752daa68
Sidhowes <sidhowes@mail.com>
2021-03-14 10:11:43 +0000

    changed recipe id

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ def recipe_detail(recipe_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), 
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
